9c198eff055d69dd7643119b7c1b3107bd7443da
Ryan Siegenthaler <r.d.siegs@protonmail.com>
2022-12-22 19:16:08 -0800

    newscript

--- integ.py
+++ integ.py
@@ -1,68 +1,18 @@
-#!/data/data/com.termux/files/usr/bin/env python3
-"""
-Integrity Check                                            UTF-8
-Tuesday, June 28th, 2022                                   RD Siegs
-"""
 import os as os
-import fileinput
 import subprocess, bash
-import PySimpleGUI
+import sys
 
 from bash import bash
 from subprocess import PIPE, Popen
 
-#List of files to be scanned
-files = []
-
-#File scanner (w3resources)
-root = input("Which directory do you want scanned? ")
-for entry in os.scandir(root):
-   if entry.is_dir():
-       typ = 'dir'
-   elif entry.is_file():
-       typ = 'file'
-   elif entry.is_symlink():
-       typ = 'link'
-   else:
-       typ = 'unknown'
-   print('{name} {typ}'.format(
-       name=entry.name,
-       typ=typ,
-   ))
-
-#the scan function is equal to files, and the list is equal to the function. Can use interchanageably
-os.scandir(root) == files
+path = input("Which directory would you like scanned? ")
 
 
-#append scan to list and print it
-files.append(root)
-print(files)
-
-
-bash('bash integrity.sh')
-
-filename = sg.popup_get_file('Enter the file you wish to process')
-sg.popup('You entered', filename)
-
-bash('bash integrity.sh' + filename)
-
-"""
-#CODE COMMENTED OUT
-#Making list into string and printing it
-s = "-"
-x = s.join(files)
-
-
-#Using bash module to run the bash script to scan python list of files
-integrity = bash('bash integrity.sh' + x)
-"""
-
-"""
-#CODE COMMENTED OUT
-ls = bash('ls . ' )
-sc = bash('bash integrity.sh')
-
-"""
-
-
-
+obj = os.scandir(path)
+for entry in obj :
+    if entry.is_file():
+        print(entry.name)
+        stdoutOrigin=sys.stdout 
+        sys.stdout = open("files.txt", "w")
+        sys.stdout.close()
+        sys.stdout=stdoutOrigin
